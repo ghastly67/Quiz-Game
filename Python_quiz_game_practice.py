55012c6d12e002bd1358c4f4b200b0c28608637f
ghastly67 <128911116+ghastly67@users.noreply.github.com>
2024-02-02 11:41:17 -0800

    Add files via upload

---
+++ Python_quiz_game_practice.py
@@ -0,0 +1,47 @@
+print("Doug's quiz game project 1")
+name = input("what's your name? ")
+name
+print("Question 1:")
+answer = input("what's 1+1? ")
+if answer == "2":
+    print("Correct!")
+else:
+    print(f"The answer is '2', not {answer!r}")
+    
+print("Question 2:")
+answer = input("What's 3x3? ")
+if answer =="9":
+    print("Correct!")
+else:
+    print(f"The answer is '2', not {answer!r}")
+    
+print("Question 3:")
+answer = input("What's 6x5? ")
+if answer =="30? ":
+    print("Correct!")
+else:print(f"The answer is '30', not {answer!r}")
+
+# Question 4:
+
+print("Question 4:")
+print("What is 1 + 1" )
+answer_1 = input("a)1\nb)2\nc)3\nd)4\n:")
+if answer_1.lower() == "b" or answer_1.lower() == "2":
+    print("Correct")
+       
+else:
+    print("Incorrect, 1 + 1 is 2")
+    print("Question 5:")
+    
+# Question 5:
+print("Question 5:")
+print("where does doug to got school?" )
+answer_1 = input("a)lrcc\nb)rutgers\nc)yale\n)nowhere\n:")
+if answer_1. lower()=="a" or answer_1.lower()=="lrcc":
+    print("Correct")
+else:
+    print("Incorrect, Doug goes to lrcc")
+
+
+
+    
